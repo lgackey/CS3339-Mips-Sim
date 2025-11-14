b57d99bd874f0a3b9d8cde2088b2781f93e74e58
jxt15 <jxt15@txstate.edu>
2025-11-13 19:58:22 -0600

    Reformatted and finished add, addi, sub, and makeBinary

--- src/alu/ALU.cpp
+++ src/alu/ALU.cpp
@@ -1,9 +1,61 @@
 #include "ALU.h"
+#include <cstdint>
 
-int ALU::add(int a, int b) { return a + b; }
-int ALU::sub(int a, int b) { return a - b; }
-int ALU::mul(int a, int b) { return a * b; }
-int ALU::bitwise_and(int a, int b) { return a & b; }
-int ALU::bitwise_or(int a, int b) { return a | b; }
-int ALU::sll(int value, int shamt) { return value << shamt; }
-int ALU::srl(int value, int shamt) { return static_cast<unsigned int>(value) >> shamt; }
+int ALU::add(int regArr[], int reg1, int reg2, int reg3) {
+    regArr[reg1] = regArr[reg2] + regArr[reg3];
+    return regArr[reg1];
+}
+
+int ALU::addi(int regArr[], int reg1, int reg2, int num) {
+    regArr[reg1] = regArr[reg2] + num;
+    return regArr[reg1];
+}
+
+int ALU::sub(int regArr[], int reg1, int reg2, int reg3) {
+    regArr[reg1] = regArr[reg2] - regArr[reg3];
+    return regArr[reg1];
+}
+
+int ALU::mul(int a, int b) {
+    return a * b;
+}
+
+int ALU::bitwise_and(int a, int b) {
+    return a & b;
+}
+
+int ALU::bitwise_or(int a, int b) {
+    return a | b;
+}
+
+int ALU::sll(int value, int shamt) {
+    return value << shamt;
+}
+
+int ALU::srl(int value, int shamt) {
+    return static_cast<unsigned int>(value) >> shamt;
+}
+
+void ALU::lw() {
+
+}
+
+void ALU::sw() {
+
+}
+
+std::bitset<32> makeBinary(int num) {
+    uint32_t temp_bit_value;
+    memcpy(&temp_bit_value, &num, sizeof(temp_bit_value));
+    std::bitset<32> binary_form(temp_bit_value);
+    return binary_form;
+}
+
+int ALU::makeInteger(std::bitset<32> binary) {
+    return 0;
+}
+
+
+
+
+
--- src/alu/ALU.h
+++ src/alu/ALU.h
@@ -1,12 +1,26 @@
-#pragma once
+#ifndef ALU_H
+#define ALU_H
+#include <bitset>
 
 class ALU {
 public:
-    int add(int a, int b);
-    int sub(int a, int b);
+    int add(int regArr[], int reg1, int reg2, int reg3);
+    int addi(int regArr[], int reg1, int reg2, int num);
+    int sub(int regArr[], int reg1, int reg2, int reg3);
     int mul(int a, int b);
+
     int bitwise_and(int a, int b);
     int bitwise_or(int a, int b);
+
     int sll(int value, int shamt);
     int srl(int value, int shamt);
+
+    void lw();
+    void sw();
+
+    std::bitset<32> makeBinary(int num);
+    int makeInteger(std::bitset<32> binary);
+
 };
+
+#endif
