114db7a08e6c03ca1a4b239c7209cfb6ecbb682e
jxt15 <jxt15@txstate.edu>
2025-11-28 17:38:06 -0600

    finished the output for the register

--- src/register/RegisterFile.cpp
+++ src/register/RegisterFile.cpp
@@ -1,4 +1,7 @@
 #include "RegisterFile.h"
+#include <bitset>
+#include <cstdint>
+
 
 const std::string regNames[32] = {"zero", "at", "v0", "v1", "a0", "a1", "a2", "a3",
                                 "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7",
@@ -27,10 +30,19 @@ void RegisterFile::reset() {
     regArr.fill(0);
 }
 
+//code to convert an int to binary
+std::bitset<32> makeBinary(int n) {
+    uint32_t temp_bit_value;
+    memcpy(&temp_bit_value, &n, sizeof(temp_bit_value));
+    std::bitset<32> binary_form(temp_bit_value);
+    return binary_form;
+}
+
 // Print all registers (useful for debug mode)
 void RegisterFile::print() const {
-    std::cout << "Register File State:\n";
+    std::cout << "Register File State:" << std::endl;
+    std::cout << "Name\tValue\tBinary Value" << std::endl;
     for (int i = 0; i < 32; ++i) {
-        std::cout << regNames[i] << ": " << regArr[i] << "\n";
+        std::cout << regNames[i] << "\t" << regArr[i] << "\t" << makeBinary(regArr[i]) << std::endl;
     }
 }
