da2dbb2226298709b238359b1b5fbf3840319bfa
jxt15 <jxt15@txstate.edu>
2025-11-28 21:23:48 -0600

    fixed memory and adjusted the output

--- src/memory/Memory.h
+++ src/memory/Memory.h
@@ -2,17 +2,21 @@
 #include <vector>
 #include <iostream>
 #include <stdexcept>
+#include <cstdint>
 
 class Memory {
 private:
-    std::vector<int> mem; // Memory represented as a vector of integers
+    std::vector<uint32_t> memoryVec; // Memory represented as a vector of integers
+    int size;
 
 public:
-    Memory(int size = 1024);   // default 1024 words
-    int loadWord(int address) const;
-    void storeWord(int address, int value);
+    Memory () : size(1024) , memoryVec(1024,0) {}
+    Memory(int s): size(s), memoryVec(s,0) {}   // default 1024 words
+    int LW(int address) const;
+    void SW(int address, int value);
     // Reset all memory to 0
     void reset();
+    void memResize(int s);
 
     // Print memory contents (for debug mode)
     void print() const;
